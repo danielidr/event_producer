f6c5db56bcff6f3e384c43c9701765c7048c9d36
Danielid Ramirez <danielid132@gmail.com>
2021-06-03 22:29:44 -0400

    Modifying group show

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-    has_many :concerts
+    has_many :concerts, dependent: :destroy
     enum group_type: [ :Band, :Women, :Men]
 
     def to_s
@@ -9,5 +9,21 @@ class Group < ApplicationRecord
     def concerts_count
         concerts.count
     end
+
+    def concerts_this_month
+        this_month = Time.now.month
+        Concert.where('extract(month from concert_date) = ?', this_month).count
+    end
+
+    def last_concert
+        Concert.maximum(:concert_date).strftime("%Y,%B,%A")
+    end
     
+    def max_audience
+        Concert.maximum(:audience)
+    end
+    
+    def max_duration
+        Concert.maximum(:duration)
+    end
 end
